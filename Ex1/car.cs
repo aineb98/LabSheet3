4f70280baf2112b5d4e8e1c48d64a1b747de8556
s00176951 <s00176951@LAB-B1041-13>
2019-10-01 12:59:29 +0100

    rest

---
+++ Ex1/car.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex1
+{
+    public class car
+    {
+        private string make;
+
+        public string Make
+        {
+            get { return this.make; }
+            set { this.make = value; }
+
+        }
+
+        private string _model;
+
+        public string Model
+        {
+            get { return _model; }
+            set { _model= value; }
+        }
+
+    }
+}
+
