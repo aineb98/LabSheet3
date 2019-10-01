e9488db37580350cdedfaa84ea5ef2e05f2e17fa
s00176951 <s00176951@LAB-B1041-13>
2019-10-01 12:40:45 +0100

    cash reg setup

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -10,6 +10,16 @@ namespace Ex1
     {
         static void Main(string[] args)
         {
+            CashRegister cr1 = new CashRegister();
+
+            Console.WriteLine($"Adding an item worth 2.70 to Cash register 1 ");
+            Console.WriteLine($"Adding an item worth 3.45 to Cash register 1 ");
+            Console.WriteLine($"Adding an item worth 5.97 to Cash register 1 ");
+
+            cr1.AddItem(2.70);
+            cr1.AddItem(3.45);
+            cr1.AddItem(5.97);
+
         }
     }
 }
