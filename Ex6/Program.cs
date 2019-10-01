53ebd805a210ffcf6a26aed4e92c32ffe5870a1d
Ann Walsh <S00176951@mail.itsligo.ie>
2019-10-01 16:57:12 +0100

    last

---
+++ Ex6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lunch lunch1, lunch2, lunch3, lunch4, lunch5;
+            lunch1 = new Lunch();
+            lunch2 = new Lunch();
+            lunch3 = new Lunch();
+            lunch4 = new Lunch();
+            lunch5 = new Lunch();
+
+            lunch1.Entree = "hamburger";
+            lunch1.Side = "fries";
+            lunch1.Drink = "cola;";
+
+        }
+    }
+}
